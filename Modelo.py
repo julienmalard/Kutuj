771c73afc2e7b2e5c70296f01403f31304a74371
Julien Malard <julien.malard@mail.mcgill.ca>
2015-12-24 18:21:35 -0500

    Primeras versiones de funciones de validación y de calibración

--- Modelo.py
+++ Modelo.py
@@ -1,5 +1,7 @@
 import numpy as np
-import scipy as sp
+import scipy.stats as estad
+import scipy.optimize as optimizar
+import math as mat
 
 
 class Control(object):
@@ -12,16 +14,52 @@ class Modelo(object):
     def __init__(símismo, varsx, vary):
         símismo.varsX = varsx
         símismo.varY = vary
-        símismo.datos = []
+        símismo.datosX = []
+        símismo.datosY = []
+        símismo.pesos_vars = None
 
-    def añadir_varx(símismo, varx):
-        símismo.varsX.append(varx)
-        for año, datos_año in enumerate(varx.datos):
-            símismo.datos = np.vstack((símismo.datos[año], datos_año))
+    def actualizar_datos(símismo):
+        símismo.datosX = []
+        for varx in símismo.varsX:
+            for año, datos in enumerate(varx.datos):
+                if año >= len(símismo.datosX):
+                    símismo.datosX.append([])
+                símismo.datosX[año].append([datos])
+        símismo.datosY = símismo.varY.datos
 
-    def calibrar(símismo):
-        pesos =
-        pesos_calibrados = sp.minimize(símismo.predecir, pesos)
+    def calibrar(símismo, datos_x, datos_y):
+        def calc_error_intervalos(pesos_var):
+            distribs = []
+            pesos = []
+            y_preds = []
+            for n_año, año in enumerate(datos_x):
+                x_conocido = datos_x.copy
+                y_conocido = datos_y.copy
+                x_pred = x_conocido.pop(n_año)
+                y_pred = y_conocido.pop(n_año)
+
+                distrib, pesos_años = símismo.predecir(x_pred, x_conocido, y_conocido, pesos_var)
+
+                y_preds.append(y_pred)
+                distribs.append(np.array(distrib))
+                pesos.append(pesos_años)
+
+            predicciones = (np.array(distribs), np.array(pesos))
+            error = símismo.validar(predicciones, np.array(y_preds))[0]
+
+            return error
+
+        pesos_var_inic = np.repeat([1], datos_x[0].shape[0])
+        pesos_calibrados = optimizar.minimize(calc_error_intervalos, pesos_var_inic).x
+
+        return pesos_calibrados
+
+    def calibrar_todos_días(símismo, datos_x, datos_y):
+        pesos_vars_diarios = np.zeros((365, datos_x[0].shape[0]))
+        for i in range(365):
+            pesos_vars_diarios[i] = símismo.calibrar(datos_x[:i+1], datos_y[:i+1])
+
+        símismo.pesos_vars = pesos_vars_diarios
 
     @staticmethod
     def predecir(datos_x_actuales, datos_x, datos_y, pesos_vars):
@@ -29,7 +67,7 @@ class Modelo(object):
         def calc_dist(datos1, datos2, pesos=pesos_vars):
             peso_año = 0
             for n_var, dato in enumerate(datos1):
-                simil = sp.wilcoxon(dato, datos2[n_var])
+                simil = (1-estad.wilcoxon(dato, datos2[n_var]).pvalue)
                 peso_año += simil ** pesos[n_var]
             return peso_año
 
@@ -40,30 +78,24 @@ class Modelo(object):
         predicciones = [datos_y, pesos_años]
         return predicciones
 
-
-
-
-        predicciones = datos_y[len(datos_x_act - 1)]
-        pesos_predicciones = pesos_años
-
-        distrib_predic = [predicciones, pesos_predicciones]
-
-        return distrib_predic
-
-
-
     @staticmethod
     def validar(predicciones, obs):
-        porcentil_datos = np.inverse_percentile(obs, predicciones[0], weights=predicciones[1])
-        x = unique(porcentil_datos)
-        y = []
-        for porcentil in x:
-            y.append(np.sum(porcentil_datos <= porcentil))
+        distribuciones = predicciones[0]
+        pesos_años = predicciones[1]
+        assert len(distribuciones) == len(pesos_años) == len(obs)
+        p = np.sum(np.greater(distribuciones, 0) * pesos_años, axis=1) * 2 / np.sum(pesos_años, axis=1)
 
-        rmse = rmse_1(x,y)
+        p_único = np.unique(p)
+        p_obs = np.zeros(np.size(p_único))
+        for n, q in enumerate(p_único):
+            p_obs[n] = np.sum(p <= q) / len(p)
 
-        return rmse, [x, y]
+        recm = calc_recm(p_obs, p)
+
+        return recm, (p, p_obs)
 
 
-def rmse_1(x, y):
+def calc_recm(y, y_pred):
+    recm = mat.sqrt(np.sum(np.square(y-y_pred))/y.size)
 
+    return recm
