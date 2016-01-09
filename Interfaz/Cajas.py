28dd340b248cd1d8d6c61dc40557dad59d23e95a
Julien Malard <julien.malard@mail.mcgill.ca>
2016-01-09 12:38:10 -0500

    Update Cajas.py

--- Interfaz/Cajas.py
+++ Interfaz/Cajas.py
@@ -33,11 +33,11 @@ class CajaLeng(tk.Frame):
     def __init__(símismo, pariente):
         super().__init__(**Fm.formato_cajas)
         símismo.apli = pariente
-        símismo.BtRegreso = Bt.BotónImagen(símismo, comanda=símismo.acción_bt_regreso,
-                                           img_norm=Gr.imagen('BtRegrCent_norm'),
-                                           img_sel=Gr.imagen('BtRegrCent_sel'),
-                                           formato=Fm.formato_botones,
-                                           ubicación=Fm.ubic_BtRegrCent, tipo_ubic='place')
+        bt_regreso = Bt.BotónImagen(símismo, comanda=símismo.acción_bt_regreso,
+                                    img_norm=Gr.imagen('BtRegrCent_norm'),
+                                    img_sel=Gr.imagen('BtRegrCent_sel'),
+                                    formato=Fm.formato_botones,
+                                    ubicación=Fm.ubic_BtRegrCent, tipo_ubic='place')
         etiq = tk.Label(símismo, text='Opciones de lenguas', **Fm.formato_CbzLeng)
         etiq.place(**Fm.ubic_CbzLeng)
         símismo.place(**Fm.ubic_CjLeng)
@@ -130,28 +130,117 @@ class CajaEtp1(CjG.CajaEtapa):
 class CajaEtp2(CjG.CajaEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Variables', núm=2, total=total)
+        total_subcajas = 1
+        subcajas = [CajaSubEtp21(símismo, total=total_subcajas)]
+
+        símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp3(CjG.CajaEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Validar', núm=3, total=total)
+        
+        total_subcajas = 2
+        subcajas = [CajaSubEtp31(símismo, total=total_subcajas),
+                    CajaSubEtp32(símismo, total=total_subcajas)]
+
+        símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp4(CjG.CajaEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Predecir', núm=4, total=total)
+        total_subcajas = 1
+        subcajas = [CajaSubEtp41(símismo, total=total_subcajas)]
+        símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp5(CjG.CajaEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Optimizar', núm=5, total=total)
+        total_subcajas = 1
+        subcajas = [CajaSubEtp51(símismo, total=total_subcajas)]
+
+        símismo.especificar_subcajas(subcajas)
 
 
 class CajaSubEtp11(CjG.CajaSubEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Cargar datos', núm=1, total=total)
+        bt_cargar_bd = Bts.BotónTexto(símismo, texto='Cargar Base de Datos, comanda=símismo.acción_bt_cargar_bd,
+                                      ubicación=Fm.ubic_bt_cargar_bd, tipo_ubic='place')
+        símismo.CjErrCargarBD = Cjs.CajaError(símismo, texto='Error en cargar la base de datos',
+                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
+        símismo.caja_activable = Cjs.CajaActivable()
+        símismo.MnColTiempo = Ctrl.MenúOpciones(caja_activable, 
+        símismo.MnColHora = Ctrl.MenúOpciones(caja_activable, 
+        símismo.CjErrColTiempo = Cjs.CajaError(símismo, texto='Error en leer los datos de fechas. Verificar su base de datos.',
+                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
+        símismo.CjErrColHora = Cjs.CajaError(símismo, texto='Error en leer los datos de horas. Verificar su base de datos.',
+                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
+        
+        símismo.MnColTiempo.estab_exclusivo(símismo.MnColHora)
+        símismo.caja_activable.bloquear()
+        
+    def acción_bt_cargar_bd(símismo):
+        try:
+            cols_bd = 
+            símismo.etiq_error_cargar_bd.hide()
+        except ERROR:
+            símismo.etiq_error_cargar_bd.show()
+        símismo.caja_activable.desbloquear()
+        símismo.MnColTiempo.refrescar(cols_bd)
+        símismo.MnColHora.refrescar(cols_bd)
+        
+    def estab_col_tiempo(símismo):
+        try:
+            
+            símismo.etiq_error_col_tiempo.hide()
+        except ERROR:
+            símismo.etiq_error_col_tiempo.show()
+        if modelo tiene col_tiempo:
+            símismo.pariente.desbloquear_caja([2])
+        else:
+            símismo.pariente.bloquear_caja([2])
+            
+    def estab_col_hora(símismo):
+        try:
+            
+            símismo.etiq_error_col_hora.hide()
+        except ERROR:
+            símismo.etiq_error_col_hora.show()
 
 
 class CajaSubEtp12(CjG.CajaSubEtapa):
     def __init__(símismo, pariente, total):
         super().__init__(pariente, nombre='Especificar variables', núm=2, total=total)
+
+
+class CajaSubEtp21(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre='Variables predictores', núm=2, total=total)
+        
+
+class CajaSubEtp22(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre='Variable a predecir', núm=2, total=total)
+        
+                
+class CajaSubEtp31(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre='Calibrar', núm=2, total=total)
+        
+        
+class CajaSubEtp32(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre='Validar', núm=2, total=total)
+        
+        
+class CajaSubEtp41(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre=None, núm=2, total=total)
+        
+        
+class CajaSubEtp51(CjG.CajaSubEtapa):
+    def __init__(símismo, pariente, total):
+        super().__init__(pariente, nombre=None, núm=2, total=total)
