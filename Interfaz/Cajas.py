c79d65e2a1931eb4f9074376dd88dc2a5e628ffe
Julien Malard <julien.malard@mail.mcgill.ca>
2016-01-11 16:04:09 -0500

    Etapa 1, subetapa 1 funciona.

--- Interfaz/Cajas.py
+++ Interfaz/Cajas.py
@@ -1,14 +1,14 @@
 import tkinter as tk
+from tkinter import filedialog as diálogo
 
-from Interfaz import Formatos as Fm
-from Interfaz import Botones as Bt
-from Interfaz import Gráficos as Gr
-from Interfaz import Animaciones as Anim
 from Interfaz import CajasGenéricas as CjG
+from Interfaz import Formatos as Fm, Botones as Bt, Gráficos as Gr, Animaciones as Anim, Controles as Ctrl
+
+from Modelo import Modelo
 
 
 class CajaInic(tk.Frame):
-    def __init__(símismo, pariente):
+    def __init__(símismo):
         super().__init__(**Fm.formato_CjInic)
         símismo.logo = Gr.imagen('LogoInic')
         logo = tk.Label(símismo, image=símismo.logo, **Fm.formato_LogoInic)
@@ -33,11 +33,11 @@ class CajaLeng(tk.Frame):
     def __init__(símismo, pariente):
         super().__init__(**Fm.formato_cajas)
         símismo.apli = pariente
-        bt_regreso = Bt.BotónImagen(símismo, comanda=símismo.acción_bt_regreso,
-                                    img_norm=Gr.imagen('BtRegrCent_norm'),
-                                    img_sel=Gr.imagen('BtRegrCent_sel'),
-                                    formato=Fm.formato_botones,
-                                    ubicación=Fm.ubic_BtRegrCent, tipo_ubic='place')
+        símismo. bt_regreso = Bt.BotónImagen(símismo, comanda=símismo.acción_bt_regreso,
+                                             img_norm=Gr.imagen('BtRegrCent_norm'),
+                                             img_sel=Gr.imagen('BtRegrCent_sel'),
+                                             formato=Fm.formato_botones,
+                                             ubicación=Fm.ubic_BtRegrCent, tipo_ubic='place')
         etiq = tk.Label(símismo, text='Opciones de lenguas', **Fm.formato_CbzLeng)
         etiq.place(**Fm.ubic_CbzLeng)
         símismo.place(**Fm.ubic_CjLeng)
@@ -47,17 +47,17 @@ class CajaLeng(tk.Frame):
 
 
 class CajaCentral(tk.Frame):
-    def __init__(símismo, pariente):
+    def __init__(símismo, apli):
         super().__init__(**Fm.formato_CjCent)
-        símismo.CjCabeza = CajaCabeza(símismo, apli=pariente)
+        símismo.CjCabeza = CajaCabeza(símismo, apli=apli)
 
         símismo.ContCjEtapas = CjG.ContCajaEtps(símismo)
         núm_etapas = 5
-        símismo.CajasEtapas = [CajaEtp1(símismo.ContCjEtapas, núm_etapas),
-                               CajaEtp2(símismo.ContCjEtapas, núm_etapas),
-                               CajaEtp3(símismo.ContCjEtapas, núm_etapas),
-                               CajaEtp4(símismo.ContCjEtapas, núm_etapas),
-                               CajaEtp5(símismo.ContCjEtapas, núm_etapas)
+        símismo.CajasEtapas = [CajaEtp1(símismo.ContCjEtapas, apli, núm_etapas),
+                               CajaEtp2(símismo.ContCjEtapas, apli, núm_etapas),
+                               CajaEtp3(símismo.ContCjEtapas, apli, núm_etapas),
+                               CajaEtp4(símismo.ContCjEtapas, apli, núm_etapas),
+                               CajaEtp5(símismo.ContCjEtapas, apli, núm_etapas)
                                ]
         símismo.ContCjEtapas.establecer_cajas(símismo.CajasEtapas)
 
@@ -117,130 +117,172 @@ class CajaIzq(tk.Frame):
 
 
 class CajaEtp1(CjG.CajaEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Base de Datos', núm=1, total=total)
 
         total_subcajas = 2
-        subcajas = [CajaSubEtp11(símismo, total=total_subcajas),
-                    CajaSubEtp12(símismo, total=total_subcajas)]
+        subcajas = [CajaSubEtp11(símismo, apli, total=total_subcajas),
+                    CajaSubEtp12(símismo, apli, total=total_subcajas)]
 
         símismo.especificar_subcajas(subcajas)
+        símismo.bloquear_subcajas([2])
 
 
 class CajaEtp2(CjG.CajaEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Variables', núm=2, total=total)
-        total_subcajas = 1
-        subcajas = [CajaSubEtp21(símismo, total=total_subcajas)]
+        total_subcajas = 2
+        subcajas = [CajaSubEtp21(símismo, apli, total=total_subcajas),
+                    CajaSubEtp22(símismo, apli, total=total_subcajas)]
 
         símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp3(CjG.CajaEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Validar', núm=3, total=total)
         
         total_subcajas = 2
-        subcajas = [CajaSubEtp31(símismo, total=total_subcajas),
-                    CajaSubEtp32(símismo, total=total_subcajas)]
+        subcajas = [CajaSubEtp31(símismo, apli, total=total_subcajas),
+                    CajaSubEtp32(símismo, apli, total=total_subcajas)]
 
         símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp4(CjG.CajaEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Predecir', núm=4, total=total)
         total_subcajas = 1
-        subcajas = [CajaSubEtp41(símismo, total=total_subcajas)]
+        subcajas = [CajaSubEtp41(símismo, apli, total=total_subcajas)]
         símismo.especificar_subcajas(subcajas)
 
 
 class CajaEtp5(CjG.CajaEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Optimizar', núm=5, total=total)
         total_subcajas = 1
-        subcajas = [CajaSubEtp51(símismo, total=total_subcajas)]
+        subcajas = [CajaSubEtp51(símismo, apli, total=total_subcajas)]
 
         símismo.especificar_subcajas(subcajas)
 
 
 class CajaSubEtp11(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Cargar datos', núm=1, total=total)
-        bt_cargar_bd = Bts.BotónTexto(símismo, texto='Cargar Base de Datos, comanda=símismo.acción_bt_cargar_bd,
-                                      ubicación=Fm.ubic_bt_cargar_bd, tipo_ubic='place')
-        símismo.CjErrCargarBD = Cjs.CajaError(símismo, texto='Error en cargar la base de datos',
-                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
-        símismo.caja_activable = Cjs.CajaActivable()
-        símismo.MnColTiempo = Ctrl.MenúOpciones(caja_activable, 
-        símismo.MnColHora = Ctrl.MenúOpciones(caja_activable, 
-        símismo.CjErrColTiempo = Cjs.CajaError(símismo, texto='Error en leer los datos de fechas. Verificar su base de datos.',
-                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
-        símismo.CjErrColHora = Cjs.CajaError(símismo, texto='Error en leer los datos de horas. Verificar su base de datos.',
-                                              ubicación=Fm.ubic_CjErrCargarBD, tipo_ubic='place')
-        
-        símismo.MnColTiempo.estab_exclusivo(símismo.MnColHora)
-        símismo.caja_activable.bloquear()
+        símismo.Modelo = apli.modelo
+
+        cj_bts = tk.Frame(símismo, **Fm.formato_cajas)
+        símismo.bt_cargar_bd = Bt.BotónTexto(cj_bts, texto='Base de datos', comanda=símismo.acción_bt_cargar_bd,
+                                             formato_norm=Fm.formato_BtsCarg_norm, formato_sel=Fm.formato_BtsCarg_sel,
+                                             ubicación=Fm.ubic_BtsCarg, tipo_ubic='pack')
+        símismo.bt_cargar_pr = Bt.BotónTexto(cj_bts, texto='Proyecto existente', comanda=símismo.acción_bt_cargar_pr,
+                                             formato_norm=Fm.formato_BtsCarg_norm, formato_sel=Fm.formato_BtsCarg_sel,
+                                             ubicación=Fm.ubic_BtsCarg, tipo_ubic='pack')
+        cj_bts.place(**Fm.ubic_CjBtsCarg)
+
+        símismo.EtiqErrCargarBD = tk.Label(símismo, text='Error cargando la base de datos', **Fm.formato_etiq_error)
+
+        símismo.CjAct = CjG.CajaActivable(símismo, ubicación=Fm.ubic_CjFechaHora, tipo_ubic='place')
+
+        cj_mn_col_fecha = tk.Frame(símismo.CjAct, **Fm.formato_cajas)
+        símismo.MnColFecha = Ctrl.Menú(cj_mn_col_fecha, nombre='Columna Fecha:', opciones=[],
+                                       comanda=símismo.acción_mn_col_fecha,
+                                       ubicación=Fm.ubic_MnCol, tipo_ubic='pack')
+        cj_mn_col_fecha.pack(**Fm.ubic_CjMnCol)
+
+        cj_mn_col_hora = tk.Frame(símismo.CjAct, **Fm.formato_cajas)
+        símismo.MnColHora = Ctrl.Menú(cj_mn_col_hora, nombre='Columna Hora:', opciones=[],
+                                      comanda=símismo.acción_mn_col_hora,
+                                      ubicación=Fm.ubic_MnCol, tipo_ubic='pack')
+        cj_mn_col_hora.pack(**Fm.ubic_CjMnCol)
+
+        símismo.CjAct.especificar_objetos([símismo.MnColHora, símismo.MnColFecha])
+
+        símismo.EtiqErrColFecha = tk.Label(cj_mn_col_fecha,
+                                           text='No su pudo leer los datos de fechas. ¿Mejor le echas un vistaso a tu '
+                                                'base de datos?',
+                                           **Fm.formato_etiq_error)
+
+        símismo.EtiqErrColHora = tk.Label(cj_mn_col_hora,
+                                          text='No se pudo leer los datos de tiempo. ¿Mejor le echas un vistaso a tu '
+                                               'base de datos?',
+                                          **Fm.formato_etiq_error)
+
+        símismo.MnColFecha.estab_exclusivo(símismo.MnColHora)
+        símismo.CjAct.bloquear()
         
     def acción_bt_cargar_bd(símismo):
+        archivo_bd = diálogo.askopenfilename(filetypes=[('Formato comas', '*.csv'),
+                                                        ('Formato texto', '*.txt')],
+                                             title='Cargar base de datos')
         try:
-            cols_bd = 
-            símismo.etiq_error_cargar_bd.hide()
-        except ERROR:
-            símismo.etiq_error_cargar_bd.show()
-        símismo.caja_activable.desbloquear()
-        símismo.MnColTiempo.refrescar(cols_bd)
+            símismo.Modelo = Modelo(archivo_bd)
+            cols_bd = símismo.Modelo.base_central.nombres_cols
+            símismo.EtiqErrCargarBD.pack_forget()
+        except (ValueError, FileNotFoundError):
+            símismo.EtiqErrCargarBD.pack(**Fm.ubic_EtiqErrCargarBD)
+            return
+
+        símismo.CjAct.desbloquear()
+        símismo.MnColFecha.refrescar(cols_bd)
         símismo.MnColHora.refrescar(cols_bd)
-        
-    def estab_col_tiempo(símismo):
+
+    def acción_bt_cargar_pr(símismo):
+        archivo_bd = diálogo.askopenfilename(filetypes=[('Proyecto Kutuj', '*.kut')],
+                                             title='Cargar proyecto existente')
+        # para hacer: hacer más cosas aquí
+
+    def acción_mn_col_fecha(símismo, col):
         try:
-            
-            símismo.etiq_error_col_tiempo.hide()
-        except ERROR:
-            símismo.etiq_error_col_tiempo.show()
-        if modelo tiene col_tiempo:
-            símismo.pariente.desbloquear_caja([2])
+            símismo.Modelo.base_central.estab_col_fecha(col)
+            símismo.EtiqErrColFecha.pack_forget()
+        except ValueError:
+            símismo.EtiqErrColFecha.pack(**Fm.ubic_EtiqErrCol)
+
+        if símismo.Modelo.base_central.fechas is not None:
+            símismo.pariente.desbloquear_subcajas([2])
         else:
-            símismo.pariente.bloquear_caja([2])
-            
-    def estab_col_hora(símismo):
+            símismo.pariente.bloquear_subcajas([2])
+
+    def acción_mn_col_hora(símismo, col):
         try:
-            
-            símismo.etiq_error_col_hora.hide()
-        except ERROR:
-            símismo.etiq_error_col_hora.show()
+            símismo.Modelo.base_central.estab_col_hora(col)
+            símismo.EtiqErrColHora.pack_forget()
+        except ValueError:
+            print('Error de valor')
+            símismo.EtiqErrColHora.pack(**Fm.ubic_EtiqErrCol)
 
 
 class CajaSubEtp12(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
+    def __init__(símismo, pariente, apli, total):
         super().__init__(pariente, nombre='Especificar variables', núm=2, total=total)
 
 
 class CajaSubEtp21(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre='Variables predictores', núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre='Variables predictores', núm=1, total=total)
         
 
 class CajaSubEtp22(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre='Variable a predecir', núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre='¿Qué quieres predecir?', núm=2, total=total)
         
                 
 class CajaSubEtp31(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre='Calibrar', núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre='Sería útil calibrar...', núm=1, total=total)
         
         
 class CajaSubEtp32(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre='Validar', núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre='...y aún mejor validar', núm=2, total=total)
         
         
 class CajaSubEtp41(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre=None, núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre=None, núm=1, total=total)
         
         
 class CajaSubEtp51(CjG.CajaSubEtapa):
-    def __init__(símismo, pariente, total):
-        super().__init__(pariente, nombre=None, núm=2, total=total)
+    def __init__(símismo, pariente, apli, total):
+        super().__init__(pariente, nombre=None, núm=1, total=total)
